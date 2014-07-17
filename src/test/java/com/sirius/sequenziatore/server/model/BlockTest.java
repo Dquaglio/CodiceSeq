1b1cd1c60e7362f086403e44e52aff4162e9f3c2
dseresin <pandarosso@gmail.com>
2014-07-17 14:33:22 +0200

    test

---
+++ src/test/java/com/sirius/sequenziatore/server/model/BlockTest.java
@@ -0,0 +1,78 @@
+package com.sirius.sequenziatore.server.model;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import com.sirius.sequenziatore.server.model.Block;
+
+public class BlockTest {
+
+	private Block test;
+	
+	public void setUp() throws Exception {
+		test.setId(2);
+	}
+	@Test
+	public final void testGetId() {
+		//fail("Not yet implemented");
+		assertEquals("test errore", 2 , test.getId());
+		
+	}
+
+	@Test
+	public final void testSetId() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public final void testGetType() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public final void testSetType() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public final void testGetRequiredSteps() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public final void testSetRequiredSteps() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public final void testGetNextBlockId() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public final void testSetNextBlockId() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public final void testGetSteps() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public final void testSetSteps() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public final void testIsFirst() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public final void testSetFirst() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ src/test/java/com/sirius/sequenziatore/server/model/GeographicDataTest.java
@@ -0,0 +1,44 @@
+package com.sirius.sequenziatore.server.model;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class GeographicDataTest {
+	
+	private GeographicData classprova;
+	
+	@Before
+	public void setup() throws Exception {
+		classprova = new GeographicData();
+		classprova.setAltitude(2);
+		classprova.setDescription("prova");
+		classprova.setLatitude(2);
+		classprova.setLongitude(2);
+		classprova.setRadius(2);
+	}
+		
+	@Test
+	public final void testGetDescription() {
+		assertEquals("testok", "prova",classprova.getDescription());
+	}
+
+	@Test
+	public final void testGetLatitude() {
+		assertEquals("testok", 2,classprova.getLatitude(),0.1);
+	}
+
+	@Test
+	public final void testGetLongitude() {
+		assertEquals("testok", 2,classprova.getLatitude(),0.1);
+	}
+	@Test
+	public final void testGetAltitude() {
+		assertEquals("testok", 2,classprova.getAltitude(),0.1);
+	}
+	@Test
+	public final void testGetRadius() {
+		assertEquals("testok", 2,classprova.getRadius(),0.1);
+	}
+}
