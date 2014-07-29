92f17f6da6e33941bc574a85cb0345c939f80e32
Dquaglio <skyzzo92@gmail.com>
2014-07-29 16:14:21 +0200

    aggiunti controller e modificati service

--- src/main/java/com/sirius/sequenziatore/server/controller/common/ProcessInfoController.java
+++ src/main/java/com/sirius/sequenziatore/server/controller/common/ProcessInfoController.java
@@ -1,65 +1,51 @@
 package com.sirius.sequenziatore.server.controller.common;
 
-import java.io.File;
 import java.io.IOException;
 import java.util.List;
 
-import javax.servlet.ServletContext;
+import javax.servlet.http.HttpServletResponse;
 
-import org.apache.commons.io.FileUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.context.ServletContextAware;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.multipart.MultipartFile;
 
 import com.sirius.sequenziatore.server.model.Block;
 import com.sirius.sequenziatore.server.model.Process;
+import com.sirius.sequenziatore.server.service.ProcessInfoService;
 
 @Controller
 @RequestMapping(value="/process/{idprocess}")
-public class ProcessInfoController implements ServletContextAware {
-	public class wrapper{
-		public List<Block> getBlock() {
-			return block;
-		}
-		wrapper(){}
-		public void setBlock(List<Block> block) {
-			this.block = block;
-		}
-		List<Block> block;
-	}
-	private ServletContext servletContext;
+public class ProcessInfoController  {
+	@Autowired
+	private ProcessInfoService processInfoService;
 	//metodo che ritorna la struttura di un processo
 	@RequestMapping(method=RequestMethod.GET)
 	@ResponseBody
 	public Process getProcessInformation(@PathVariable int idprocess){
-		return null;
+		Process toBeReturned=processInfoService.getProcess(idprocess);
+		if(toBeReturned!=null){
+			return toBeReturned;
+		}
+		throw new IllegalStateException("Processo non presente");
 	}
-	//metodo e oggetto per mvc annotation driven per salvare le immagini
+	//metodo per salvare le immagini
 	@RequestMapping(value="/saveimage", method=RequestMethod.POST)
 	@ResponseBody
 	public void uploadImage(@RequestParam(value="image")MultipartFile image){
-		if(image.isEmpty()){
-			System.out.println("Immagine vuota!!!!");
+		boolean result=processInfoService.saveImage(image);
 		}
-		else{
-			System.out.println("Qualcosa cè");
-			File file=new File(servletContext.getRealPath("/")+"/images/"+"sicazzo.jpg");
-			try {
-				FileUtils.writeByteArrayToFile(file, image.getBytes());
-				System.out.println("THE END");
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-		}
+	@ExceptionHandler(IllegalStateException.class)
+	@ResponseStatus(value = HttpStatus.UNPROCESSABLE_ENTITY,   reason = "Il processo cercato non è presente")
+	public void handleException3(IllegalStateException ex, HttpServletResponse response) throws IOException{
+		//invia al client un errore 422
 	}
-	@Override
-	public void setServletContext(ServletContext servletContext) {
-		this.servletContext=servletContext;
-	}
+	
 }
--- src/main/java/com/sirius/sequenziatore/server/service/ProcessInfoService.java
+++ src/main/java/com/sirius/sequenziatore/server/service/ProcessInfoService.java
@@ -1,8 +1,45 @@
 package com.sirius.sequenziatore.server.service;
 
+import java.io.File;
+import java.io.IOException;
+
+import javax.servlet.ServletContext;
+
+import com.sirius.sequenziatore.server.model.Process;
+import com.sirius.sequenziatore.server.model.ProcessDao;
+
+import org.apache.commons.io.FileUtils;
 import org.springframework.stereotype.Service;
+import org.springframework.web.context.ServletContextAware;
+import org.springframework.web.multipart.MultipartFile;
 
 @Service
-public class ProcessInfoService {
-
+public class ProcessInfoService implements ServletContextAware{
+	private ServletContext servletContext;
+	@Override
+	public void setServletContext(ServletContext servletContext) {
+		this.servletContext=servletContext;
+	}
+	public boolean saveImage(MultipartFile image){
+		if(image.isEmpty()){
+			return false;
+		}
+		else{
+			File file=new File(servletContext.getRealPath("/")+"/images/"+"sicazzo.jpg");
+			try {
+				FileUtils.writeByteArrayToFile(file, image.getBytes());
+			} catch (IOException e) {
+				e.printStackTrace();
+				return false;
+			}
+		}
+		return true;
+	}
+	public Process getProcess(int id){
+		Process toBeReturned;
+		ProcessDao processDao;
+		toBeReturned=processDao.getProcess(id);
+		return toBeReturned;
+	}
+	
 }
