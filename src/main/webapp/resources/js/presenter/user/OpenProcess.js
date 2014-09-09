fab12742ae5a6abc79bf5814e9908fb7204a25e2
gmarcomi <gabriele.marcomin@gmail.com>
2014-09-09 20:36:35 +0200

    -Correzioni ad Open,SendData e position -Corretti errori nei template ed aggiunto i pezzi del ManageBase

--- src/main/webapp/resources/js/presenter/user/OpenProcess.js
+++ src/main/webapp/resources/js/presenter/user/OpenProcess.js
@@ -13,55 +13,84 @@ define([
     'underscore',
     'backbone',
     'presenter/BasePresenter',
-    'text!view/user/OpenProcess.html',
+    'text!view/user/openProcessTemplate.html',
     'collection/ProcessCollection'
 ], function( $, _, Backbone, BasePresenter, openProcessTemplate, ProcessCollection ){
 
-    var template= _.template(openProcessTemplate);
-    var collection= new ProcessCollection();
-
-    var extractGet=function(expression,string){
-        var result=new RegExp(expression).exec(string);
+    //metodo privato per il parsing dei valori nella querystring
+    var parseGet=function(){
+        var result = new Array();
+        var query = window.location.search.substring(1);
+        if (query){
+            //splito la querystring
+            var stringList = query.split('&');
+            var firstPart=stringList[0].split('=');
+            if(firstPart[0]=="type")
+                result[unescape(firstPart[0])]=unescape(firstPart[1]);
+        }
+        var sito = get['sito'];
         return result;
     }
 
 
-
     var OpenProcess = BasePresenter.extend({
 
-
+        session:null,
+        template: _.template(openProcessTemplate),
+        collection: new ProcessCollection({ username: "Gabriele" }),
+        id: '#processes',
+        el: $('body'),
 
         initialize: function () {
             this.constructor.__super__.createPage.call(this, "processes");
             this.listenTo(this.collection, 'all', this.render);
+            this.session = options.session;
         },
 
-
-
-        id: '#processes',
-
-        el: $('body'),
-
-        render: function() {
-            collection.fetch();
-            //variabile che contiene i processi
-            var processJson=collection.toJSON();
-            this.update();
-            var result = extractGet("#process\\?(\\w+=\\w+&)*"+param+"=(\\d{1,11})",address);
-            var type_process=null;
-            if(result=="new")
-                type_process="subscribable";
-            else if(result="ongoing")
-                type_process="just subscribed";
-
-            $(this.id).html(this.template({ processes: processJson })).enhanceWithin();
+        //renderizza il template usando i dati acquisiti dal fetching
+        render: function( options,tipo) {
+            // default value
+            var error = typeof options.error !== "undefined" ? options.error : null;
+            // template rendering and JQM css enhance
+            $(this.id).html(this.template({
+                processes: this.collection.toJSON(),
+                username: this.session.getUsername(),
+                error: error,
+                tipo:tipo
+                })).enhanceWithin();
         },
 
+        // aggiorna la collezione di processi "collection", recuperando i dati dal server
         update: function() {
-            collection.fetch();
+            var self = this;
+            var gets = parseGet.call(self);
+            //se la lunghezza dell'array risultato vuol dire che il parsing ha trovato una variabile type
+            var subscribe=null;
+            //variabile testuale per il template
+            var tipo="";
+
+            if(gets.length == 1);{
+                //?type=new
+                if(result=="new") {
+                    subscribe = true;
+                    tipo="iscrivibili"
+                }
+                //?type=ongoing
+                else if(result="ongoing"){
+                    subscribe=false;
+                    tipo="già iscritti";
+                }
+            }
+
+            self.collection.fetch({subscribe:subscribe}).done( function() {
+                self.render({},tipo);
+            }).fail( function(error) {
+                if(error.status == 0)
+                    self.render({ text: "Errore di connessione" });
+                else
+                    self.render({ text: error.status+" "+error.statusText });
+            }).always( function() { self.trigger("updated"); });
         }
-
     });
-
     return OpenProcess;
 });
--- src/main/webapp/resources/js/presenter/user/SendData.js
+++ src/main/webapp/resources/js/presenter/user/SendData.js
@@ -1,35 +1,65 @@
 define([
- 'jquery',
- 'underscore',
- 'backbone',
- 'presenter/BasePresenter',
- 'text!view/user/SendData.html',
- 'model/ProcessDataCollection',
+    'jquery',
+    'underscore',
+    'backbone',
+    'presenter/BasePresenter',
+    'text!view/user/sendDataTemplate.html',
+    'model/ProcessDataModel',
  'jquerymobile'
-], function( $, _, Backbone, BasePresenter, SendDataTemplate, ProcessDataCollection ){
+], function( $, _, Backbone, BasePresenter, SendDataTemplate,ProcessDataModel){
 
-    var process = new ProcessDataCollection();
-    var template = _.template(SendDataTemplate);
+    var SendData = BasePresenter.extend({
 
-	var SendData = BasePresenter.extend({
-
-	    //attributi
-
-
-        id: '#send_data',
-        el: $('body'),
-
-
+        session:null,
+        process:null,
+        template : _.template(SendDataTemplate),
 
         //metodi
 
         initialize: function () {
-			this.constructor.__super__.createPage.call(this, "send_data");
-			_.extend(this.events, BasePresenter.prototype.events);
+            this.session=options.session;
 		},
-
+    /*riferimento per i nomi dei dati
+    {
+        "id": 1,
+        "processId": 1,
+        "description": "Raggiungi torre archimede, dicci cosa vedi e inviaci le coordinate",
+        "nextStep": 2,
+        "requiresApproval": false,
+        "optional": true,
+        "textualData" : [
+        { "id": 1, "description": "Cosa vedi" }
+    ],
+        "numericData": null,
+        "imageData": null,
+        "geographicData": {
+        "latitude": 45.411331,
+            "longitude": 11.8876318,
+            "altitude": 17,
+            "radius": 2000
+    }
+    }
+    */
         render: function() {
+            this.step;
             process.fetch();
+            /* blocco render da adattare
+             render: function( options,tipo) {
+             // default value
+             var error = typeof options.error !== "undefined" ? options.error : null;
+             // template rendering and JQM css enhance
+             $(this.id).html(this.template({
+             processes: this.collection.toJSON(),
+             username: this.session.getUsername(),
+             error: error,
+             tipo:tipo
+             })).enhanceWithin();
+             },
+
+
+
+             */
+            //in attesa di modifiche
 		    $(this.id).html(template({
 					            text: true,
 					            position: true,
@@ -45,9 +75,32 @@ define([
         saveData:function(){
 
         }
-
 	});
-
+    //guida per inviare dati versione vecchia
+    /* Cosente di inviare al server i dati di un passo eseguito.
+     *
+     * ESEMPIO UTLIZZO
+     * // array dei valori rilevati
+     * var values = new Array();
+     * values.push({ dataId: 1, value: "testo" });
+     * values.push({ dataId: 1, value: 12.34 });
+     * values.push({ dataId: 1, imageUrl: "img/Tesoro" });
+     * values.push({ dataId: 1, latitude: 12.34 , longitude: 12.34, altitude: 12.34 });
+     *
+     * // data corrente; il formato è da concordare con i progettisti del database
+     * var now = new Date();
+     *
+     * var model = new ProcessDataModel({
+     *		stepId: 1, username: "Gabriele", dateTime: now, values: values
+     *	});
+     *
+     * // per testare si può invertire fail con save
+     * model.save().done( function() {
+     *		console.log("salvato con successo")
+     *	}).fail( function() {
+     *		console.log("errore di connessione")
+     *	});
+     */
 	return SendData;
 
 });
